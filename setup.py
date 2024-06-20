6e30b967e7312e9ec990c9b9f112a399a8872910
eyaoueslati06 <eyaoueslati.oue@gmail.com>
2024-06-20 19:47:57 +0200

    setup

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E_DOT = '-e .'
+## La ligne -e . dans requirements.txt est utilisée pour installer le projet actuel en mode éditable. Cependant, 
+##cette ligne n'est pas une exigence de package standard et ne doit pas être passée à l'argument install_requires dans la fonction setup.
 
 def get_requirements(file_path: str) -> List[str]:
     """ This function will return the list of requirements """
@@ -15,6 +17,7 @@ def get_requirements(file_path: str) -> List[str]:
 
     return requirements
 
+## Directive du Mode Éditable : La directive -e . est spécifique à pip pour le développement local et ne doit pas être incluse dans la liste des packages que setuptools essaie d'installer.
 setup(
     name='mlproject',
     version='0.0.1',
